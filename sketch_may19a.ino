739b961b8bf97a41dfa1a79dfce9af7fee214b69
bobogrand <bobogrand@nate.com>
2017-05-21 22:12:43 +0900

    Update sketch_may19a.ino

--- sketch_may19a.ino
+++ sketch_may19a.ino
@@ -1,5 +1,6 @@
 /* Arduino Code */
-
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include "arduinoFFT.h"
 
 arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
@@ -7,7 +8,7 @@ arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 These values can be changed in order to evaluate the functions
 */
 const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
-double signalFrequency = 1000;
+double signalFrequency = 100;
 double samplingFrequency = 5000;
 uint8_t amplitude = 100;
 /*
@@ -23,15 +24,39 @@ double vImag[samples];
 
 #define Theta 6.2831 //2*Pi
 
+// wifi
+const char* ssid = "GraphicsLab";
+const char* passwd = "graphics";
+const char* server_ip = "163.239.78.75";
+
 void setup()
 {
+  // WIFI CONNECT info
+
+
   Serial.begin(115200);
-//  Serial.println("Ready");
+
+
+  //WIFI connect
+  WiFi.begin(ssid,passwd);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+
 }
 
 void loop()
 {
 
+//Listen From Mic
     if(analogRead(A0) > 50){
       for (uint8_t i = 0; i < samples; i++){
         vReal[i] = analogRead(A0);
@@ -45,6 +70,30 @@ void loop()
       delay(2000);
   }
 
+/* send data to server 
+
+  WiFiClient client;
+  const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+    }
+    client.print(String("GET ") + path + temperatureString + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: keep-alive\r\n\r\n");
+  if (!client.connect(server_ip, httpPort)) {
+    Serial.println("connection failed in server");
+    return;
+  }
+
+String temp = String("GET ") + "/?value=" + temperatureString + " HTTP/1.1\r\n" +
+               "Host: " + server_ip + "\r\n" +
+               "Connection: keep-alive\r\n\r\n";
+ client.print(String("GET ") + "/?value=" + temperatureString + " HTTP/1.1\r\n" +
+               "Host: " + server_ip + "\r\n" +
+               "Connection: keep-alive\r\n\r\n");
+ */
+
 }
 
 void PrintVector(double *vData, uint8_t bufferSize, uint8_t scaleType)
@@ -55,10 +104,7 @@ void PrintVector(double *vData, uint8_t bufferSize, uint8_t scaleType)
       vData[i]=0;
     }
     uint8_t val_temp = map(vData[i],0,1000,0,255);
-
     Serial.write(val_temp);
   }
   Serial.write('\n');
 }
-
-
