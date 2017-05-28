ffeb8a64fcd80d2d4e6c4706f573a168d662f827
bobogrand <bobogrand@nate.com>
2017-05-28 23:43:13 +0900

    Add files via upload
    sendor to computer

---
+++ sender.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#define samples 5000
+// wifi
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+    if(analogRead(A0) > 50){
+      for (int i = 0; i < samples; i += 1){
+        int data;
+        data= analogRead(A0);
+        Serial.println(data);
+        delayMicroseconds(60);
+      }
+      delay(100);
+    }
+    
+}
+
+
+
